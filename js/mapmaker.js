c088737a262d54eac6b0c7b0afdbcb6857aced2a
HakkuJogai02 <bloggrog805@gmail.com>
2025-09-01 12:27:12 -0300

    Update mapmaker.js

--- js/mapmaker.js
+++ js/mapmaker.js
@@ -818,6 +818,7 @@ export class MapMaker {
                     'Heist': { img: '${env}/Gamemode_Specifics/Heist.png' },
                     'Bounty': { img: 'Global/Objectives/Bounty.png' },
                     'Brawl_Ball': { img: '${env}/Gamemode_Specifics/Brawl_Ball.png' },
+                    'Dodgebrawl': { img: 'Global/Objectives/Dodgebrawl.png' },
                     'Hot_Zone': { img: 'Global/Objectives/Hot_Zone.png', size: 7 },
                     'Snowtel_Thieves': { 
                         img: `Global/Objectives/${y > mapHeight/2 ? 'SnowtelThievesBlue' : 'SnowtelThievesRed'}.png`,
