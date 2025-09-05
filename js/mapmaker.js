d24107b9b57a67a9cfe1003f1e1491c08b865f90
She Fairy <shefairies@gmail.com>
2025-09-05 13:49:35 +0300

    Auto-preset of Basket Brawl Layout

--- js/mapmaker.js
+++ js/mapmaker.js
@@ -3486,6 +3486,24 @@ export class MapMaker {
             }
         }
 
+        if (this.mapSize === this.mapSizes.basket) {
+            // Place spawns for basket maps
+            this.mapData[6][this.mapWidth - 1] = 13;      // Red
+            this.mapData[6][0] = 12;                      // Blue
+            this.mapData[8][this.mapWidth - 1] = 13;      // Red
+            this.mapData[8][0] = 12;                      // Blue
+            this.mapData[10][this.mapWidth - 1] = 13;     // Red
+            this.mapData[10][0] = 12;                     // Blue
+
+            // Center objective
+            if (this.gamemode === 'Basket_Brawl') {
+            const centerY = Math.floor(mapHeight / 2);
+            const centerX = Math.floor(mapWidth / 2);
+            this.placeTile(centerX, centerY, 14, false); // Place objective tile
+            }
+        }
+
+
         // Showdown-specific Brawl Ball setup
         if (this.mapSize === this.mapSizes.showdown && this.gamemode === 'Brawl_Ball') {
             const centerX = Math.floor(mapWidth / 2);
